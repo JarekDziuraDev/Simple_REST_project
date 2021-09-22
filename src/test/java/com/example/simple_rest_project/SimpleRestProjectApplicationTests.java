e7bdb254a2f264cf26f46edeab0412b32f70d26f
Jarek Dziura <jarek4313@gmail.com>
2021-09-22 12:36:44 +0200

    init - start project, add basic dependiences

---
+++ src/test/java/com/example/simple_rest_project/SimpleRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_rest_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
