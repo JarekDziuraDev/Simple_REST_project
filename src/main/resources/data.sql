a878eca30e0137aa05694103563483f8439350e0
JarekDziuraDev <jarek.dziura.dev@gamil.com>
2021-09-25 17:58:06 +0200

    addition new data.sql values for tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,11 @@
-insert into post(id, title, content) values (1, 'Post number 1', 'Content post 1');
+insert into post(id, title, content, created) values (1, 'Post number 1', 'Content post 1', '2021-09-24T12:01:01.122334');
+insert into post(id, title, content, created) values (2, 'Post number 2', 'Content post 2', '2021-09-24T12:01:01.122334');
+insert into post(id, title, content, created) values (3, 'Post number 3', 'Content post 3', '2021-09-24T12:01:01.122334');
+insert into post(id, title, content, created) values (4, 'Post number 4', 'Content post 4', '2021-09-24T12:01:01.122334');
+insert into post(id, title, content, created) values (5, 'Post number 5', 'Content post 5', '2021-09-24T12:01:01.122334');
+
+insert into comment(id, post_id, content, created) values (1, 1, 'Comment 1 to post 1', '2021-09-24T12:01:01.122334');
+insert into comment(id, post_id, content, created) values (2, 1, 'Comment 2 to post 1', '2021-09-24T12:05:01.122334');
+insert into comment(id, post_id, content, created) values (3, 1, 'Comment 3 to post 1', '2021-09-24T12:10:01.122334');
+insert into comment(id, post_id, content, created) values (4, 1, 'Comment 4 to post 1', '2021-09-24T12:15:01.122334');
+insert into comment(id, post_id, content, created) values (5, 1, 'Comment 5 to post 1', '2021-09-24T12:20:01.122334');
