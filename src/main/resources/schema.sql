fd1de83faef28bcadc79db860dcedbaa2b224208
JarekDziuraDev <jarek.dziura.dev@gamil.com>
2021-09-25 17:51:29 +0200

    addition new table comment with relation to post

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,17 @@
 create table POST (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(400) NOT NULL,
-    content VARCHAR(2000) NULL
+    content VARCHAR(2000) NULL,
+    created timestamp
 );
+
+create table COMMENT (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    post_id BIGINT NOT NULL,
+    content VARCHAR(2000) NULL,
+    created timestamp
+);
+
+alter table comment
+    add constraint comment_post_id
+    foreign key (post_id) references  post(id)
